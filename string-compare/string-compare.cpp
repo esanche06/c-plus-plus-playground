8016c6fe94f3debbae0cd80e2e7c23313d5f81e9
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-02-26 15:48:28 -0800

    Add string-compare

---
+++ string-compare/string-compare.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+int main() {
+    string codeword = "codecademy";
+    string answer = "codecademy";
+
+    if (codeword == answer) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
