9549ce0ad225d842b2e021d4d74729d1e2e68390
Ethan Sanchez <ethan.dsanch@gmail.com>
2023-09-24 16:55:50 -0700

    Update practice-swap with pointer-by-ref swap

--- practice-swap/practice-swap.cpp
+++ practice-swap/practice-swap.cpp
@@ -2,21 +2,43 @@
 
 using namespace std;
 
+// Let's follow a "boxes in cubbies" analogy for these examples...
+
+// Here, we take out the boxes a* and b*, and swap their contents, a and b...
 template <typename T>
-void swap_practice(T& a, T& b) {
-    T temp = a;
+void value_by_ref_swap_practice(T& a, T& b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+// Here, we take out the boxes a* and b*, take out their contents a and b, and swap the *cubby*
+// each box is in, but the contents of each box is in the same cubby it started in...
+template <typename T>
+void pointer_by_ref_swap_practice(T*& a, T*& b) {
+    T *temp;
+    temp = a;
     a = b;
     b = temp;
 }
 
 int main() {
     int a = 0, b = 1;
+    int *aPtr = &a, *bPtr = &b;
 
     cout << a << " VS. " << b << "\n";
+    cout << aPtr << " VS. " << bPtr << "\n";
 
-    swap_practice(a, b);
+    value_by_ref_swap_practice(a, b);
 
     cout << a << " VS. " << b << "\n";
+    cout << aPtr << " VS. " << bPtr << "\n";
+
+    pointer_by_ref_swap_practice(aPtr, bPtr);
+
+    cout << a << " VS. " << b << "\n";
+    cout << aPtr << " VS. " << bPtr << "\n";
 
     return 0;
 }
